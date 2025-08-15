b0bade18a7962ebacf21032ae82c5e465c09d6d2
Gowtham-Nakka <142660342+Gowtham-Nakka@users.noreply.github.com>
2025-08-15 17:42:41 +0530

    Add files via upload

---
+++ Java8_features_Assignment/src/com/java/ANameFinder.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class ANameFinder {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Anu", "Bhanu", "Charitha", "Shyam", "Arjun");
+
+        List<String> aNames = names.stream()
+                                   .filter(name -> name.startsWith("A"))
+                                   .collect(Collectors.toList());
+
+        aNames.forEach(System.out::println);
+    }
+}
