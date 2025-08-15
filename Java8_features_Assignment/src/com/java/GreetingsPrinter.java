d065153262680af43f92b44585e6ddf0888f6ef3
Nakka Gowtham <gowthamnakka@example.com>
2025-08-15 17:38:43 +0530

    Added NewFolder

---
+++ Java8_features_Assignment/src/com/java/GreetingsPrinter.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class GreetingsPrinter {
+    public static void main(String[] args) {
+        MessagePrinter printer = message -> System.out.println("Greeting: " + message);
+        greetUser("Welcome to Wipro!", printer);
+    }
+
+    @FunctionalInterface
+    interface MessagePrinter {
+        void print(String message);
+    }
+
+    public static void greetUser(String message, MessagePrinter printer) {
+        printer.print(message);
+    }
+}
---
+++ Java8_features_Assignment/src/com/java/NameSorter.java
@@ -0,0 +1,11 @@
+package com.java;
+
+import java.util.*;
+
+public class NameSorter {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Akki", "Anu", "Ram", "Rashmitha", "Geetanjali");
+        names.sort((name1, name2) -> name1.compareTo(name2));
+        names.forEach(System.out::println);
+    }
+}
