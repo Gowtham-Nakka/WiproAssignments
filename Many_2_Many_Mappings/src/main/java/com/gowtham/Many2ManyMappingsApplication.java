e6565561ec5ec059acb849606aaea97cb8789a76
Gowtham-Nakka <142660342+Gowtham-Nakka@users.noreply.github.com>
2025-08-16 09:13:55 +0530

    Add files via upload

---
+++ Many_2_Many_Mappings/src/main/java/com/gowtham/Many2ManyMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.gowtham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Many2ManyMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Many2ManyMappingsApplication.class, args);
+	}
+
+}
---
+++ Many_2_Many_Mappings/src/test/java/com/gowtham/Many2ManyMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowtham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Many2ManyMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
