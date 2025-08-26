23029a05c1560c082421b3ca956ad4752c641496
Gowtham-Nakka <142660342+Gowtham-Nakka@users.noreply.github.com>
2025-08-26 15:29:48 +0530

    Add files via upload

---
+++ ZomatoDeliveryAgent/src/main/java/com/wipro/gow/ZomatoAgentDeliveryService.java
@@ -0,0 +1,18 @@
+package com.wipro.gow;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ZomatoAgentDeliveryService {
+
+	private final KafkaTemplate<String,String> kafkaTemplate;
+	public void updateDeliveryAgentLocation(String string) {
+		
+		kafkaTemplate.send("zomato-delivery-agent",string);
+	}
+
+}
